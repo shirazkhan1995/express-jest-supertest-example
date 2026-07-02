98978918be925c1282e01ae0cd5939807299b5af
Shiraz <shirazkhan1995@gmail.com>
2026-07-02 14:57:52 +0530

    Initial commit

--- routers/users.spec.js
+++ routers/users.spec.js
@@ -5,16 +5,15 @@ const request = supertest(app);
 
 describe("User routes", () => {
   describe("GET /users", () => {
-    test("should return an array of user objects", async (done) => {
+    test("should return an array of user objects", async () => {
       const res = await request.get("/users");
       expect(res.status).toBe(200);
       expect(res.body.length).toEqual(5);
-      done();
     });
   });
 
   describe("GET /users/:id", () => {
-    test("should respond with a specific user", async (done) => {
+    test("should respond with a specific user", async () => {
       const res = await request.get("/users/1");
       expect(res.status).toEqual(200);
       expect(res.body).toEqual({
@@ -22,15 +21,12 @@ describe("User routes", () => {
         firstName: "Irene",
         lastName: "de Nicolo",
       });
-
-      done();
     });
 
-    test("should respond with 404 if the user is not found", async (done) => {
+    test("should respond with 404 if the user is not found", async () => {
       const res = await request.get("/users/99");
       expect(res.status).toEqual(404);
       expect(res.body.message).toEqual("User not found");
-      done();
     });
   });
 });
